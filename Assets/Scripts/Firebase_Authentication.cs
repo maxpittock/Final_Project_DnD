7a11485581d520bb41cde18b77fb4896d829ff8e
maxpittock <33483789+maxpittock@users.noreply.github.com>
2022-05-14 23:52:47 +0100

    Firebase installed

---
+++ Assets/Scripts/Firebase_Authentication.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Firebase;
+using Firebase.Auth;
+using TMPro;
+
+
+public class Firebase_Authentication : MonoBehaviour
+{
+
+    //Firebase variables
+    [Header("Firebase")]
+    public DependencyStatus dependencyStatus;
+    public FirebaseAuth auth;    
+    public FirebaseUser User;
+
+    //Login variables
+    [Header("Login")]
+    public TMP_InputField emailLoginField;
+    public TMP_InputField passwordLoginField;
+    public TMP_Text warningLoginText;
+    public TMP_Text confirmLoginText;
+
+    //Register variables
+    [Header("Register")]
+    public TMP_InputField usernameRegisterField;
+    public TMP_InputField emailRegisterField;
+    public TMP_InputField passwordRegisterField;
+    public TMP_InputField passwordRegisterVerifyField;
+    public TMP_Text warningRegisterText;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
